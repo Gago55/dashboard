6c837c88d877e96d6f7956663c58e351f7709c01
gago55 <xgagik8@gmail.com>
2020-01-09 15:20:07 +0400

    Add 'users' in state and 'addUser' mutation

--- store/index.js
+++ store/index.js
@@ -1,5 +1,8 @@
 export const state = () => ({
-
+    users : [
+        { username : "root" , password : "1234"},
+        { username : "admin" , password : "admin"}
+    ]
 })
 
 export const actions = {
@@ -7,7 +10,9 @@ export const actions = {
 }
 
 export const mutations = {
-
+    addUser(state , {username , password}){
+        state.users.push({username , password})
+    }
 }
 
 export const getters = {
