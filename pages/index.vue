9067d49e35b0e307ac29bcf0e5d0a94f669859dd
gago55 <xgagik8@gmail.com>
2020-01-11 17:44:13 +0400

    Add App Bar and cryptoChart

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,54 @@
 <template>
- <div>
-   <h2>{{username}}</h2>
-   <button @click="logout">Log Out</button>
+  <div>
+    <v-app-bar dark>
+      <v-toolbar-title>Dashboard</v-toolbar-title>
+      <v-spacer />
+      <span class="mr-3">{{username}}</span>
+      <v-btn @click="logout">Log Out</v-btn>
+    </v-app-bar>
 
-   <div v-if="!this.$store.getters.getIsAuth">
-     <!-- For update when isAuth changed -->
-   </div>
- </div>
+    <v-row align="center" justify="center">
+      <cryptoChart class="mt-5"/>
+    </v-row>
+
+    <div v-if="!this.$store.getters.getIsAuth">
+      <!-- For update when isAuth changed -->
+    </div>
+  </div>
 </template>
 
 <script>
+import cryptoChart from "@/components/cryptoChart"
 
 export default {
-  data : () => ({
-    username : process.browser ? localStorage.getItem('username') : ""
+  components:{
+    cryptoChart
+  },
+  data: () => ({
+    username: process.browser ? localStorage.getItem("username") : "" ,
+
   }),
-  created(){
-    if(process.browser){
-      if(!localStorage.getItem("username")){
-        this.$router.push({path : "/login"})
+  created() {
+    if (process.browser) {
+      if (!localStorage.getItem("username")) {
+        this.$router.push({ path: "/login" });
       }
     }
   },
-  updated(){
-    if(!localStorage.getItem("username")){
-        this.$router.push({path : "/login"})
-      }
+  updated() {
+    if (!this.$store.getters.getIsAuth) {
+      this.$router.push({ path: "/login" });
+    }
   },
-  methods:{
-    onclick(){
-      this.$store.commit('addUser' , { username : "gag" , password : "fuck"})
+  methods: {
+    onclick() {
+      this.$store.commit("addUser", { username: "gag", password: "fuck" });
     },
-    logout(){
-      localStorage.removeItem("username")
-      this.$store.commit("setIsAuth" , false)
-    }
+    logout() {
+      localStorage.removeItem("username");
+      this.$store.commit("setIsAuth", false);
+    },
+    
   }
-}
+};
 </script>
